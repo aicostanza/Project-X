03a096c30be52885fa1305fd392732dcd95af16c
aicostanza <shaunlouiserumbaoa@gmail.com>
2026-09-09 00:57:08 +0800

    Added Match API + Match ID showcase

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-//Server Side JS
-
 require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
+
 app.use(cors());
 
 console.log(process.env.RIOT_API_KEY);
@@ -14,87 +14,122 @@ app.get("/riot/account", (req, res) => {
     const tagLine = req.query.tagLine;
     const server = req.query.server;
 
-    let GameName;
-    let TagLine;
+    let LOLToken = process.env.RIOT_API_KEY;
+    let LOLAPISMNv4;
+    let LOLAPILGv4;
+    let LOLMATCHv5;
 
+    const LOLAPIACv1 = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`; // Account Info + PUUID
 
-    const LOLAPIACv1 = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`;
+    console.log(gameName, tagLine, server);
 
-    console.log(gameName, tagLine);
+    //
+    // ACCOUNT V1 / GET ACCOUNT BY GAMENAME + TAG LINE API CALL
+    //
+    // URL: https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}
+    //
 
     fetch(LOLAPIACv1, {
         headers: {
-            "X-Riot-Token": process.env.RIOT_API_KEY
+            "X-Riot-Token": LOLToken
         }}) .then(response => {
-            console.log(response.status); 
+            console.log(response.status);
             return response.json()
+        }) .then(data => {
+            console.log(data);
+
+            const puuid = data.puuid;
+            LOLAPISMNv4 = `https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`; // Summoner Info + Profile Icon + Level
+            LOLAPILGv4 = `https://${server}.api.riotgames.com/lol/league/v4/entries/by-puuid/${puuid}`; // LOL RANKED INFO
+            LOLMATCHv5 = `https://sea.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids`; // LOL MATCH HISTORY
+        }) 
+        
+    //
+    // RETURNED DATA: PUUID
+    //
+        
+    // 
+    // LEAGUE SUMMONERV4 / GET SUMMONER ICON + LEVEL API CALL
+    // 
+    // URL: https://${server}.api.riotgames.com/lol/league/v4/entries/by-puuid/${puuid}
+    //
+
+    .then(() => {
+        return fetch(LOLAPISMNv4, {
+        headers: {
+            "X-Riot-Token": LOLToken
+        }}) .then(response => {
+                console.log(response.status);
+                return response.json();
+        }) .then(data => {
+                console.log(data);
+
+                profileIconId = data.profileIconId;
+                summonerLevel = data.summonerLevel;
+            }) 
+    //
+    // RETURNED DATA: Profile Icon + Summoner Level
+    //
+
+    //
+    // LEAGUE V4 / GET RANKED INFO API CALL 
+    //
+    // URL: https://${server}.api.riotgames.com/lol/league/v4/entries/by-puuid/${puuid}
+    //
+
+    .then(() => {
+    return fetch(LOLAPILGv4, {
+        headers: {
+        "X-Riot-Token": LOLToken
+        }}) .then(response => {
+            console.log(response.status);
+            return response.json();
         })  .then(data => {
             console.log(data);
-            const puuid = data.puuid;
 
-            GameName = data.gameName;
-            TagLine = data.tagLine;
-
-            const LOLAPIACv4 = `https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`
-
-            fetch(LOLAPIACv4, {
-                headers: {
-                    "X-Riot-Token": process.env.RIOT_API_KEY
-                }}) .then(response => {
-                    console.log(response.status);
-                    return response.json()
+            SR = data.find(entry => entry.queueType === "RANKED_SOLO_5x5");
+            FLEX = data.find(entry => entry.queueType === "RANKED_FLEX_SR");
+            FIVE = data.find(entry => entry.queueType === "RANKED_PREMADE_5x5");
+            CLASSIC = data.find(entry => entry.queueType === "JADE_RANKED_SOLO_5x5");
+            }) 
+    // 
+    // RETURNED DATA: Ranked Info
+    //
+    
+    //
+    // MATCH V5 / GET MATCH ID API CALL
+    //
+    // URL: https://${server}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20
+    //
+    
+    .then(() => {
+        return fetch(LOLMATCHv5, { 
+        headers: {
+            "X-Riot-Token": LOLToken
+            }}) .then(response => {
+                console.log(response.status);
+                return response.json();
+            })  .then(data => {
+                console.log(data);
+                
+                res.json({
+                    gameName: gameName,
+                    tagLine: tagLine,
+                    server: server,
+                    profileIconId: profileIconId,
+                    summonerLevel: summonerLevel,
+                    SR: SR,
+                    FLEX: FLEX,
+                    FIVE: FIVE,
+                    CLASSIC: CLASSIC,
+                    matchHistory: data
+                    });
                 })
-                    .then(data => {
-                        console.log(data);
+            })
+        }) 
+    });
+});
 
-                        profileIconId = data.profileIconId;
-                        summonerLevel = data.summonerLevel;
-
-                            const LOLAPIACv4RK = `https://${server}.api.riotgames.com/lol/league/v4/entries/by-puuid/${puuid}`
-
-                            fetch(LOLAPIACv4RK, {
-                                headers: {
-                                    "X-Riot-Token": process.env.RIOT_API_KEY
-                                }}) .then(response => {
-                                    console.log(response.status);
-
-                                    return response.json()
-                                }) .then(data => {
-                                    console.log(data);
-
-                                        SR = data.find(rank => rank.queueType === "RANKED_SOLO_5x5");
-                                        FLEX = data.find(rank => rank.queueType === "RANKED_FLEX_SR");
-                                        FIVE = data.find(rank => rank.queueType === "RANKED_PREMADE_5x5");
-                                        CLASSIC = data.find(rank => rank.queueType === "JADE_RANKED_SOLO_5x5"); 
-
-
-                                    })
-
-                                    LOLMATCHv5 = `https://sea.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`
-                                    fetch(LOLMATCHv5, {
-                                        headers: {
-                                            "X-Riot-Token": process.env.RIOT_API_KEY
-                                        }
-                                    }) .then(response => {
-                                        console.log(response.status);
-                                        return response.json()
-                                    }) .then(data => {
-                                        console.log(data);
-                                        res.json({
-
-                                        gameName: GameName,
-                                        tagLine: TagLine,
-                                        profileIconId,
-                                        summonerLevel,
-                                        SR,
-                                        FLEX,
-                                        FIVE,
-                                        CLASSIC,
-                                })
-                            })
-      
-                    }) 
-        })      
 app.listen(2106, () => {
-    console.log("https://localhost:2106");
+    console.log("Server running on http://localhost:2106");
 });
